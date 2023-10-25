7cd2c7bd49543657b465cce6afa4cdc0a56c8e78
Andriamahazosoa Dimbinantenaina Rabearivony <arabearivony@aimsammi.org>
2023-10-25 18:00:35 +0200

    Implemented the #at method

--- lib/linkedlist.rb
+++ lib/linkedlist.rb
@@ -28,4 +28,38 @@ class LinkedList
       @head = new_node
     end
   end
+  
+  def size
+    return 0 if @head.value.nil? && @head.next_node.nil?
+    
+    len = 1
+    current_node = @head
+    until current_node.next_node.nil? 
+      current_node = current_node.next_node
+      len += 1
+    end
+    len
+  end
+
+  def tail
+    return @head if @head.value.nil? && @head.next_node.nil?
+    
+    current_node = @head
+    until current_node.next_node.nil?
+       current_node = current_node.next_node
+    end
+    current_node
+  end
+
+  def at(index)
+    return 'Invalid index' if !index.is_a?(Integer) || index.abs >= size
+    return @head if index == 0
+    current_node = @head
+    stop_index = (index > 0 ? index : (size + index))
+    (0...stop_index).each {|i| current_node = current_node.next_node}
+    current_node
+  end
+
+  def pop 
+  end
 end
--- main.rb
+++ main.rb
@@ -1,16 +1,28 @@
 require_relative "lib/linkedlist"
-require_relative "lib/linkedlist"
+
+sep = "="*20
 
 list = LinkedList.new()
 
-puts "Initial linked list"
+puts "Initial linked list, with size #{list.size}"
 p list
+puts sep
 
 (1..5).each {|x| list.append(x)}
-puts 'After appending some nodes'
+puts "After appending some nodes, the size is: #{list.size}"
 p list
+puts sep
 
-
-list.prepend(-111)
-puts 'After prepending a negative element'
+list.prepend(0)
+puts "After prepending 0, the size is #{list.size}"
 p list
+puts sep
+
+puts "The tail of the list is"
+p list.tail
+puts sep
+
+index = -2
+puts "At index #{index}  we have the node:"
+p list.at(index)
+puts sep
